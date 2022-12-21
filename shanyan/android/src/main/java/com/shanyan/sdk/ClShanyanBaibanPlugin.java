b56d81bcab97a66e4f633fd8da73d61ecc96f310
wanggang <wanggang@cnebula.net>
2022-12-21 17:57:48 +0800

    更新内部android原生SDK

--- shanyan/android/src/main/java/com/shanyan/sdk/ClShanyanBaibanPlugin.java
+++ shanyan/android/src/main/java/com/shanyan/sdk/ClShanyanBaibanPlugin.java
@@ -59,7 +59,7 @@ public class ClShanyanBaibanPlugin implements FlutterPlugin, MethodCallHandler {
                 setDebug(call);
             } else if ("getShanyanVersion".equals(call.method)) {
                 //获取SDK版本
-                result.success("2.4.4.3");
+                result.success("2.4.4.5");
             } else if ("init".equals(call.method)) {
                 //初始化
                 init(call, result);
